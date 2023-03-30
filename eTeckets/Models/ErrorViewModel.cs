cf8d5d03658efb7dc01d62358652673ee851abeb
dell <dell@DESKTOP-541R0BD>
2023-03-30 12:28:47 +0530

    Add project files.

---
+++ eTeckets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTeckets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
